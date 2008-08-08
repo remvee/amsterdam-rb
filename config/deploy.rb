63229b916870b5af73b0e710870e36345acef980
remco <remco@dad1564c-276e-11dd-9e82-efc310838ca2>
2008-08-08 10:12:57 +0000

    cap2
    git-svn-id: http://svn.rubyenrails.nl/amsterdam-rb@9 dad1564c-276e-11dd-9e82-efc310838ca2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,12 @@
-require 'deprec/recipes'
-
 set :application, "amsterdam-rb"
 set :domain, "amsterdam-rb.org"
+set :deploy_to, "/u/apps/#{application}"
+set :rails_env, "production"
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+
+set :scm, :subversion
+set :repository, "--username remco http://svn.rubyenrails.nl/amsterdam-rb"
 
 role :web, domain
 role :app, domain
 role :db, domain, :primary => true
-
-set :repository,  "--username remco http://svn.rubyenrails.nl/amsterdam-rb"
-set :rails_env, "production"
-
-set :mongrel_user, 'amsterdam-rb'
-set :mongrel_group, 'deploy'
-
-set :apache_server_name, domain
-set :apache_proxy_port, 3050
-set :apache_proxy_servers, 3
