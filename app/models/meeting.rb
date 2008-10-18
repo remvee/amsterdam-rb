268ea3da5b32ca040bb4e0891503888c588e4657
remco <remco@dad1564c-276e-11dd-9e82-efc310838ca2>
2008-10-18 13:18:55 +0000

    introduce Meeting.next, constrain start_at to last monday of month and default start_at to next last monday of month
    git-svn-id: http://svn.rubyenrails.nl/amsterdam-rb@23 dad1564c-276e-11dd-9e82-efc310838ca2

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,18 @@
 class Meeting < ActiveRecord::Base
+  validate do |meeting|
+    d = meeting.start_at.to_date
+    if d != d.next_last_monday_of_the_month
+      meeting.errors.add(:start_at, 'should be last monday of month')
+    end
+  end
+
+  def self.next(today = Date.today)
+    next_start_on = today.next_last_monday_of_the_month
+    next_meeting = Meeting.first(:conditions => ['start_at >= ? AND start_at < ?', next_start_on.to_time, (next_start_on + 1.day).to_time])
+    next_meeting || Meeting.new(:start_at => next_start_on.to_time + 20.hours)
+  end
+
+  def start_at
+    super || (Date.today.next_last_monday_of_the_month.to_time + 20.hours)
+  end
 end
--- test/unit/meeting_test.rb
+++ test/unit/meeting_test.rb
@@ -1,4 +1,26 @@
 require 'test_helper'
 
 class MeetingTest < ActiveSupport::TestCase
+  def test_next_creates_new_meeting_if_not_available
+    m = Meeting.next
+    assert_kind_of Meeting, m
+    assert_equal Date.today.next_last_monday_of_the_month, m.start_at.to_date
+  end
+
+  def test_next_finds_existing_meeting_if_available
+    m = Meeting.next(Date.new(2008, 5, 1))
+    assert_equal meetings(:one), m
+  end
+
+  def test_start_at_should_be_last_monday_of_month
+    assert_no_difference 'Meeting.count' do
+      m = Meeting.new(:start_at => Date.new(2008, 10, 20))
+      m.save
+      assert_not_nil m.errors.on(:start_at)
+    end
+  end
+
+  def test_start_at_defaults_to_next_last_monday_of_month_at_20h
+    assert_equal Date.today.next_last_monday_of_the_month.to_time + 20.hours, Meeting.new.start_at
+  end
 end
