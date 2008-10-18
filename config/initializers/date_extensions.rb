1c671a51151d3ec3cf3746539579a6dbe6bb5096
remco <remco@dad1564c-276e-11dd-9e82-efc310838ca2>
2008-10-18 13:18:53 +0000

    moved next last monday calculation to date_extensions
    git-svn-id: http://svn.rubyenrails.nl/amsterdam-rb@22 dad1564c-276e-11dd-9e82-efc310838ca2

---
+++ config/initializers/date_extensions.rb
@@ -0,0 +1,21 @@
+require 'activesupport'
+
+module DateExtensions
+  def next_last_monday_of_the_month
+    t, m, l = self, month, nil
+
+    loop do
+      l = t if t.wday == 1 # monday
+      t += 1.day
+
+      if m != t.month
+        break if l
+        m = t.month
+      end
+    end
+
+    l.to_date
+  end
+end
+
+Date.send(:include, DateExtensions)
