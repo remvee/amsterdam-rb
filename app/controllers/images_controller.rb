4e073720fa665312127e2283649d13309e7022de
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-09-22 17:33:15 +0200

    page caching doesn't work on heroku

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,4 @@
 class ImagesController < ApplicationController
-  caches_page :show
-
   def show
     @image = Image.find(params[:id])
     send_data @image.data, :type => @image.content_type, :disposition => 'inline'
